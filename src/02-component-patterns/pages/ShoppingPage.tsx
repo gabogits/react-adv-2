bde318a7dca77a9b9fe0c9cb347d2ae4f2b81491
Gabriel Martinez Miranda <gabriel@kubofinanciero.com>
2022-09-27 15:18:51 -0500

    some shanges

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -4,7 +4,7 @@ import {
   ProductButtons,
   ProductCard,
 } from "../components/";
-import "../styles/custom-styles.css";
+
 import { products } from "../data/products";
 
 const ShoppingPage = () => {
@@ -21,29 +21,17 @@ const ShoppingPage = () => {
         }}
       >
         <ProductCard
-          style={{ backgroundColor: "#107ec8" }}
           product={products[0]}
-          className="bg-dark"
           key={products[0].id}
           initialValues={{
             count: 4,
-            maxCount: 10,
           }}
         >
           {({ reset, increaseBy, isMaxCountReached, count }) => (
             <>
-              <ProductImage className="custom-image" activeClass="holi" />
-              <ProductTitle className="text-white" />
-              <ProductButtons
-                className="custom-buttons"
-                style={{ display: "flex", justifyContent: "end" }}
-              />
-              <button onClick={reset}>reset</button>
-              <button onClick={() => increaseBy(-2)}>-2</button>
-              {!isMaxCountReached && (
-                <button onClick={() => increaseBy(2)}>+2</button>
-              )}
-              <span>{count}</span>
+              <ProductImage />
+              <ProductTitle />
+              <ProductButtons />
             </>
           )}
         </ProductCard>
